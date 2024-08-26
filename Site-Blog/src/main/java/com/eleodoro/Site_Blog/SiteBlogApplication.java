ed7aa4e58d88df9724d6bac50ba0138d4e9d6f03
MiguelFSoda <165044124+MiguelFSoda@users.noreply.github.com>
2024-08-26 12:15:07 -0300

    Add files via upload

---
+++ Site-Blog/src/main/java/com/eleodoro/Site_Blog/SiteBlogApplication.java
@@ -0,0 +1,13 @@
+package com.eleodoro.Site_Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteBlogApplication.class, args);
+	}
+
+}
---
+++ Site-Blog/src/test/java/com/eleodoro/Site_Blog/SiteBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.Site_Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
